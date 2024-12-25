99eec8954f29692778c8858b70f44324ee109b82
Robun_Gid <robungidorig@gmail.com>
2024-12-25 19:26:18 +0300

    🛒 Cart bump effect added

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CartIcon } from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 import { CartContext } from '../../store/cart-context';
 
 export const HeaderCartButton = ({ onClick }) => {
+	const [isButtonAnimated, setIsButtonAnimated] = useState(false);
 	const cartContext = useContext(CartContext);
 
 	const cartItemsCount = cartContext.items.reduce((currentCount, item) => {
 		return currentCount + item.count;
 	}, 0);
 
+	const buttonClasses = `${styles.button} ${isButtonAnimated ? styles.bump : ''}`;
+
+	useEffect(() => {
+		if (cartContext.items.length == 0) {
+			return;
+		}
+		setIsButtonAnimated(true);
+
+		const timer = setTimeout(() => {
+			setIsButtonAnimated(false);
+		}, 300);
+
+		return () => clearTimeout(timer);
+	}, [cartContext.items]);
+
 	return (
-		<button className={styles.button} onClick={onClick}>
+		<button className={buttonClasses} onClick={onClick}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
