3850a457aac1b000965bbf6ecbb7fd5512dee1ee
Robun_Gid <robungidorig@gmail.com>
2024-12-25 04:33:04 +0300

    🛒 Cart button component added

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,12 @@
 import booksStoreImage from '../../assets/books_store_large.jpg';
 import styles from './Header.module.css';
-
+import { HeaderCartButton } from './HeaderCartButton';
 export const Header = () => {
 	return (
 		<>
 			<header className={styles.header}>
 				<h1>BooksWP</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={styles['main-image']}>
 				<img src={booksStoreImage} alt='Books store image' />
