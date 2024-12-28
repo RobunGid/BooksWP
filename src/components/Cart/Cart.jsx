b781f30e9d092fee151dd4e4920b71c7c4736226
Robun_Gid <robungidorig@gmail.com>
2024-12-28 03:25:59 +0300

    🎁 Order form added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Modal } from '../UI/Modal';
 import styles from './Cart.module.css';
 import { CartContext } from '../../store/cart-context';
 import { CartItem } from './CartItem';
+import { SubmitOrder } from './SubmitOrder';
 
 export const Cart = ({ onHideCart }) => {
+	const [isSubmitOrderAvailable, setIsSubmitOrderAvailable] = useState(false);
+
 	const cartContext = useContext(CartContext);
 
 	const totalAmount = `$${Math.abs(cartContext.totalAmount).toFixed(2)}`;
@@ -17,6 +20,10 @@ export const Cart = ({ onHideCart }) => {
 
 	const handleRemoveCartItem = (id) => {
 		cartContext.removeItem(id);
+	};
+
+	const handleOrder = () => {
+		setIsSubmitOrderAvailable(true);
 	};
 
 	const cartItems = (
@@ -36,6 +43,19 @@ export const Cart = ({ onHideCart }) => {
 		</ul>
 	);
 
+	const modalButtons = (
+		<div className={styles.actions}>
+			<button onClick={onHideCart} className={styles['button--alt']}>
+				Close
+			</button>
+			{hasItems && (
+				<button className={styles['button']} onClick={handleOrder}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
 	return (
 		<Modal onClose={onHideCart}>
 			{cartItems}
@@ -43,12 +63,8 @@ export const Cart = ({ onHideCart }) => {
 				<span>Total</span>
 				<span>{totalAmount}</span>
 			</div>
-			<div className={styles.actions}>
-				<button onClick={onHideCart} className={styles['button--alt']}>
-					Close
-				</button>
-				{hasItems && <button className={styles['button']}>Order</button>}
-			</div>
+			{isSubmitOrderAvailable && <SubmitOrder onCancel={onHideCart} />}
+			{!isSubmitOrderAvailable && modalButtons}
 		</Modal>
 	);
 };
---
+++ src/components/Cart/SubmitOrder.jsx
@@ -0,0 +1,82 @@
+import styles from './SubmitOrder.module.css';
+import { useRef, useState } from 'react';
+
+const isInputValid = (inputValue) => inputValue.trim() !== '';
+
+export const SubmitOrder = ({ onCancel }) => {
+	const [formValidity, setFormValidity] = useState({
+		name: true,
+		city: true,
+		address: true,
+	});
+
+	const nameInputRef = useRef();
+	const cityInputRef = useRef();
+	const addressInputRef = useRef();
+
+	const handleConfirmOrder = (event) => {
+		event.preventDefault();
+
+		const enteredName = nameInputRef.current.value;
+		const enteredCity = cityInputRef.current.value;
+		const enteredAddress = addressInputRef.current.value;
+
+		const isEnteredNameValid = isInputValid(enteredName);
+		const isEnteredCityValid = isInputValid(enteredCity);
+		const isEnteredAddressValid = isInputValid(enteredAddress);
+
+		setFormValidity({
+			name: isEnteredNameValid,
+			city: isEnteredCityValid,
+			address: isEnteredAddressValid,
+		});
+
+		const isFormValid =
+			isEnteredNameValid && isEnteredCityValid && isEnteredAddressValid;
+
+		if (!isFormValid) {
+			return;
+		}
+	};
+
+	const nameInputClasses = `${styles.control} ${
+		formValidity.name ? '' : styles.invalid
+	}`;
+
+	const cityInputClasses = `${styles.control} ${
+		formValidity.city ? '' : styles.invalid
+	}`;
+
+	const addressInputClasses = `${styles.control} ${
+		formValidity.address ? '' : styles.invalid
+	}`;
+
+	return (
+		<form className={styles.form} onSubmit={handleConfirmOrder}>
+			<div className={nameInputClasses}>
+				<label htmlFor='name'>Name</label>
+				<input type='text' id='name' ref={nameInputRef} />
+				{formValidity.name || <p>Please, enter correct name</p>}
+			</div>
+
+			<div className={cityInputClasses}>
+				<label htmlFor='city'>City name</label>
+				<input type='text' id='city' ref={cityInputRef} />
+				{formValidity.city || <p>Please, enter correct city name</p>}
+			</div>
+
+			<div className={addressInputClasses}>
+				<label htmlFor='address'>Address</label>
+				<input type='text' id='address' ref={addressInputRef} />
+				{formValidity.address || <p>Please, enter correct address</p>}
+			</div>
+
+			<div className={styles.actions}>
+				<button className={styles.submit}>Confirm order</button>
+				<button onClick={onCancel} type='button'>
+					Cancel order
+				</button>
+			</div>
+		</form>
+	);
+};
